46147f518f3a274bfe0f57106dead135249db136
huseyinmertkok <huseyinmert.kok@gazi.edu.tr>
2021-07-05 17:06:24 +0300

    Update MemoryModel.scala

--- app/models/MemoryModel.scala
+++ app/models/MemoryModel.scala
@@ -1,20 +1,66 @@
 package models
 
-import scala.collection.mutable
+import java.sql.{DriverManager, ResultSet}
 
+import scala.collection.mutable
 object MemoryModel {
-  private val users = mutable.Map[String,String]("jakixcar" -> "123")
+  private val users = mutable.Map[String,String]()
+
   def validateUser(username:String,password:String):Boolean={
-    users.get(username).map(_ == password).getOrElse(false)
+    println("Postgres connector")
+    var check: Boolean = false
+    var usname: String = ""
+    var passw: String = ""
+    classOf[org.postgresql.Driver]
+    val con_str = "jdbc:postgresql://localhost/proje1?user=admin&password=123"
+    val conn = DriverManager.getConnection(con_str)
+    try {
+      val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+      val rs = stm.executeQuery("SELECT * from main_table")
+
+      while(rs.next || !check) {
+        usname = rs.getString("username")
+        passw = rs.getString("pass")
+        check = usname.equals(username) && password.equals(password)
+      }
+      check
+    } finally {
+      conn.close()
+    }
 
   }
 
   def createUser(username:String,password:String):Boolean={
-    if(users.contains((username))) false
-    else {
-      users(username) = password
-      true
+    println("Postgres connector")
+    var check: Boolean = false
+    var usname: String = ""
+    var passw: String = ""
+    classOf[org.postgresql.Driver]
+    val con_str = "jdbc:postgresql://localhost/proje1?user=admin&password=123"
+    val conn = DriverManager.getConnection(con_str)
+    try {
+      val stm = conn.createStatement(ResultSet.TYPE_FORWARD_ONLY, ResultSet.CONCUR_READ_ONLY)
+      val rs = stm.executeQuery("SELECT * from main_table")
+
+      while(rs.next || !check) {
+        usname = rs.getString("username")
+        passw = rs.getString("pass")
+        check = usname.equals(username) && password.equals(password)
+      }
+
+      if(check) false
+      else {
+        val rs = stm.executeQuery("INSERT INTO main_table VALUES(usname,passw)")
+        true
+      }
+    } finally {
+      conn.close()
     }
+
   }
+
+
+
+
 
 }
